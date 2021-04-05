4216f41862cd6c107a9406f808abc1155cd9c194
Nicolas Dalbard <dalbard.nicolas@me.com>
2021-04-06 01:12:05 +0200

    first commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.8
+RUN pip install flask numpy scipy
+ADD https://nicod.s3.fr-par.scw.cloud/dataset.pkl ./data/dataset.pkl
+ADD https://nicod.s3.fr-par.scw.cloud/mat.pkl ./data/mat.pkl
+COPY . ./app
+EXPOSE 80
+CMD python ./app/main.py
---
+++ main.py
@@ -0,0 +1,42 @@
+import flask
+from flask import request
+from flask import jsonify
+import pickle
+import numpy as np
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+tracks = []
+uri_to_index = {}
+index_to_uri = {}
+mat = None
+
+def load():
+	if mat == None:
+		dataset = pickle.load(open('data/dataset.pkl', 'rb'))
+		mat = pickle.load(open('data/mat.pkl', 'rb'))
+		
+		tracks = dataset[0]
+		uri_to_index = dataset[1]
+		index_to_uri = dataset[2]
+
+def similar_music(track_uri):
+	load()
+	if not track_uri in uri_to_index: return []
+
+	i0 = uri_to_index[track_uri]
+
+	print(tracks[index_to_uri[i0]]['track_name'])
+	top = np.argpartition(mat[i0,:].toarray()[0], -10)[-10:]
+	print([tracks[index_to_uri[i]]['track_name']+' - '+tracks[index_to_uri[i]]['artist_name'] for i in top])
+
+	return [index_to_uri[i] for i in top]
+
+@app.route('/', methods=['GET'])
+def home():
+	res = similar_music(request.args.get('uri'))
+	return jsonify(res)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', debug=True, port=80, use_reloader=False)
